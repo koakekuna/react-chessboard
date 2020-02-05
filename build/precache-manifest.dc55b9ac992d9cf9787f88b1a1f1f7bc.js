57b8a793d9c0d136965ffab6968c9b849a729622
Koa Kekuna <kkekuna@dudnyk.com>
2020-02-05 09:17:34 -0500

    create build for netlify deploy

---
+++ build/precache-manifest.dc55b9ac992d9cf9787f88b1a1f1f7bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c45f282363d91f964e2f",
+    "url": "/static/css/main.504572e0.chunk.css"
+  },
+  {
+    "revision": "c45f282363d91f964e2f",
+    "url": "/static/js/main.67343b29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b47e5204f23dd456b1e",
+    "url": "/static/js/2.20b55d67.chunk.js"
+  },
+  {
+    "revision": "53483171e7e612d358c6a36bae3dc5ff",
+    "url": "/index.html"
+  }
+];
